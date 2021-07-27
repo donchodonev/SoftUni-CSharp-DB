68b8c5797ab1a216e09b3153138d629ff1fccec9
donchodonev <dummyacc9292@gmail.com>
2021-07-27 09:41:05 +0300

    added another exercise solution

---
+++ 2.CRUD/14.Sort Employees Table.sql
@@ -0,0 +1,8 @@
+-- Write a SQL query to sort all records in the Employees table by the following criteria: 
+-- •	First by salary in decreasing order
+-- •	Then by first name alphabetically
+-- •	Then by last name descending
+-- •	Then by middle name alphabetically
+
+SELECT * FROM [Employees]
+ORDER BY [Salary] DESC,[FirstName],[LastName] DESC,[MiddleName]
