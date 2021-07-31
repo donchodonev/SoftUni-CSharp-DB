0c2991f2596a4ae0f77c512cea6a6da580bc7ef4
donchodonev <dummyacc9292@gmail.com>
2021-07-31 12:40:27 +0300

    added another exercise solution

---
+++ 4.Built-in functions/11.Find All Employees with Rank 2.sql
@@ -0,0 +1,10 @@
+SELECT * FROM
+
+(SELECT [EmployeeID], [FirstName],[LastName],[Salary]
+    , DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+    FROM [Employees]
+    WHERE [Salary] >= 10000
+    AND [Salary] <= 50000
+) AS Ranked
+WHERE [Rank] = 2
+ORDER BY [Salary] DESC
