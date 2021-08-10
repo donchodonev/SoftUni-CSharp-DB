c699f6d3903241216e9cdce3cc0390a648fd592e
donchodonev <dummyacc9292@gmail.com>
2021-08-10 18:56:59 +0300

    added another exercise solution

--- 5.SUBQUERIES AND JOINS/05. Employees Without Projects.sql
+++
@@ -1,5 +0,0 @@
-SELECT TOP(3) E.[EmployeeID], FirstName FROM EmployeesProjects AS ep
-	FULL JOIN Employees AS e
-	ON ep.[EmployeeID] = e.EmployeeID
-		WHERE EP.[ProjectID] IS NULL
-			ORDER BY EP.[EmployeeID]
