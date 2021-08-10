32ed69dcfc46df98e0a79f0a721860672e2f7757
donchodonev <dummyacc9292@gmail.com>
2021-08-10 19:02:38 +0300

    fixing missing items due to git issue

--- 5.SUBQUERIES AND JOINS/06. Employees Hired After.sql
+++
@@ -1,8 +0,0 @@
-SELECT [FirstName],[LastName],[HireDate],D.[Name] as DepartmentName
-	FROM Employees AS E
-	JOIN Departments AS D
-	ON E.[DepartmentID] = D.[DepartmentID]
-		WHERE D.[Name] IN ('Sales','Finance')
-		AND
-		HireDate > '1999-1-1'
-			ORDER BY [HireDate]
